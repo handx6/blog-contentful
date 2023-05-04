9b5e2b8c9f3070ff6471fe98e4ac24aa3dc44c23
Martin Nicolas <mjld.nicolas@gmail.com>
2023-05-04 19:18:56 +0200

    hero ready for about and prepared

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Layout from '@/components/layout/Layout';
+import { createClient } from 'contentful';
 
-export default function About() {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFULL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFULL_ACCESS_TOKEN,
+  });
+  
+  const postsFooter = await client.getEntries({
+    content_type: "escciBlog",
+    limit: 2,
+  });
+  // 3 - on envoie la data dans les props
+  return {
+    props: {
+      postsFooter: postsFooter.items,
+    },
+  };
+}
+export default function About({postsFooter}) {
   return (
     <>
-        <Layout page="About">
+        <Layout page={["Home", "About"]} postsFooter={postsFooter}>
           <h1>About</h1>
         </Layout>
     </>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Layout from "@/components/layout/Layout";
+import { createClient } from "contentful";
 
-export default function Contact() {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFULL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFULL_ACCESS_TOKEN,
+  });
+
+  const postsFooter = await client.getEntries({
+    content_type: "escciBlog",
+    limit: 2,
+  });
+  // 3 - on envoie la data dans les props
+  return {
+    props: {
+      postsFooter: postsFooter.items,
+    },
+  };
+}
+
+export default function Contact({ postsFooter }) {
   return (
     <>
       <>
-        <Layout page="Contact">
+        <Layout page={["Home", "Contact"]} postsFooter={postsFooter}>
           <h1>Contact</h1>
         </Layout>
       </>
